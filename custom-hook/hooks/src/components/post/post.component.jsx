b666765e120af63d98d14c12029c8bf4f344bcb2
Bryan Valerio <bryanvale91@gmail.com>
2020-05-15 15:56:38 -0600

    Create project for useState

---
+++ custom-hook/hooks/src/components/post/post.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Card from '../card/card.component';
+import useFetch from '../../effects/use-fecth.effect';
+
+const Post = ({ postId }) => {
+  const post = useFetch(
+    `https://jsonplaceholder.typicode.com/posts?id=${postId}`,
+  );
+
+  return (
+    <Card>
+      {post ? (
+        <div>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ) : (
+        <p>No post found</p>
+      )}
+    </Card>
+  );
+};
+export default Post;
